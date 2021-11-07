5a3a3eafc422af272f854acc2ac7ccf9bdf8679c
mhrpoya <mehrpoyakhosravi49@gmail.com>
2021-11-07 17:31:06 +0330

    change1

--- View.py
+++ View.py
@@ -1,25 +1,26 @@
 from tkinter import Tk, Button, Entry, Label
 
-
 class LoginView(Tk):
-    def __init__(self):
+    def __init__(self, callback1):
         super(LoginView, self).__init__()
+        self.callback1 = callback1
         x = self.winfo_screenwidth()
-        self.geometry(f"{x}x200")
-        b = Button(self, text="login")
+        self.geometry("300x300+550+150")
+        b = Button(self, text="login" , command= self.login_callback)
         b.grid(row=3, column=2)
-        user = Entry(self)
+        self.user = user = Entry(self)
         user.grid(row=1, column=2)
-        pass_ = Entry(self)
+        self.pass_ = pass_ = Entry(self)
         pass_.grid(row=2, column=2)
         lbl_user = Label(self, text="user")
         lbl_user.grid(row=1, column=1)
         lbl_pass = Label(self, text="pass")
         lbl_pass.grid(row=2, column=1)
-        lbl_hm = Label(self, text="HIDDEN MASSAGE")
-        lbl_hm.grid(column=4, row=4)
+        lbl_hm = Label(self, text="HIDDEN MESSAGE")
+        lbl_hm.grid(column=2, row=4)
+    def login_callback(self):
+        u = self.user.get()
+        p = self.pass_.get()
+        self.callback1(u, p)
 
 
-root = LoginView()
-root2 = LoginView()
-root.mainloop()
